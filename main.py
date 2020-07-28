a1153dea241cbab5783ce49f471f41c81d609685
Cigashamwa byamungu <ebyamungu28@gmail.com>
2020-07-28 17:21:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,33 @@
+#class Testobject:
+  #x = 1
+  #y = 2
+  #z = 4
+
+  #def math():
+    #TestObject x = z + y / z
+    #return x
+
+
+#print( TestObject.y )
+
+class Book:
+  numberOfPages = 0
+  Title:""
+  Author:""
+  Editior:""
+  Illistrator:""
+  Fic: False
+
+  shelf = []
+  for r in range(10):
+    b.book
+    b.numberOfPages = int(input("Number of Pages ?"))
+    b.Title = str(input("Name of the Book ?"))
+    b.Editior = str(input("Name of the Editior ?"))
+    b.Author = str(input("Name of the Author ?"))
+    b.Illistrator = str(input("Name of the Illistrator ?"))
+    b.Fic = str(input( "True or False ?"))
+
+for r in range(3):
+  print range....
+
